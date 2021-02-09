bbafa27a53f8de8320c51535495dd881c638de36
ashwiniksingh <43825183+ashwiniksingh@users.noreply.github.com>
2021-02-10 00:41:07 +0530

    v2
    v2

--- booksandmovies/settings.py
+++ booksandmovies/settings.py
@@ -114,8 +114,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+VENV_PATH = os.path.dirname(BASE_DIR)
+STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
--- booksandmoviesapp/urls.py
+++ booksandmoviesapp/urls.py
@@ -5,10 +5,16 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add/index', views.index, name='index'),
+    path('add/add', views.addView, name='add'),
     path('add/',views.addView, name='add'),
     path('store', views.store, name='store'),
     path('view/<int:pk>', views.viewBookorMovie, name='viewBookorMovie'),
     path('delete/<int:pk>', views.deleteBookorMovie, name='deleteBookorMovie'),
     path('update/<int:pk>',views.updateBookorMovie, name='updateBookorMovie'),
-    path('edit/<int:pk>', views.update, name='edit')
+    path('edit/<int:pk>', views.update, name='edit'),
+    path('view/index', views.index, name='index'),
+    path('view/add', views.addView, name='add'),
+    path('update/index',views.index, name='index'),
+    path('update/add',views.addView, name='add'),
 ]
