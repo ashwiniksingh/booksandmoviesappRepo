eb5acf3c1d63ca7b21fe8636e0c762081b159715
ashwiniksingh <43825183+ashwiniksingh@users.noreply.github.com>
2021-02-16 22:38:04 +0530

    dirs

--- booksandmovies/settings.py
+++ booksandmovies/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'booksandmovies.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +118,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 VENV_PATH = os.path.dirname(BASE_DIR)
 STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
